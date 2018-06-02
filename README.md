52fbf75d6bc7ae92c4b47cdfd449403fb5008f3d
James Doyle <james.doyle@sycle.net>
2018-06-02 16:30:26 -0700

    add support for vars and make the theme overall more flexible

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# Fish Theme: clearance
+# Fish Theme: clearance2
 
-A minimalist [fish shell](http://fishshell.com/) theme for people who use git
+A minimalist [fish shell](http://fishshell.com/) theme for people who use git. Forked from [cseelus/clearance-fish](https://github.com/cseelus/clearance-fish).
 
-![clearance theme](https://raw.github.com/cseelus/clearance-fish/master/clearance-fish_preview.png)
+![clearance2 theme](https://raw.github.com/cseelus/clearance-fish/master/preview.png)
 
-## Like it?
+## Config
 
-Check out other versions of this theme for OSX Terminal or VIM!
+My main difference in this theme is that you can tweak the config.
+
+This is the default setup:
+
+* `set -g theme_prompt_char_normal '⟩ '`
+* `set -g theme_prompt_virtual_env_char_begin '['`
+* `set -g theme_prompt_virtual_env_char_end ']'`
+* `set -g theme_prompt_git_char_begin '('`
+* `set -g theme_prompt_git_char_end ')'`
+* `set -g theme_prompt_git_is_dirty '±'`
+* `set -g theme_prompt_divider ' · '`
+* `set -g theme_prompt_newline yes`
+* `set -g theme_prompt_full_path yes`
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,9 +1,19 @@
-# name: clearance
+# name: clearance2
 # ---------------
-# Based on idan. Display the following bits on the left:
+# Based on clearance. Display the following bits on the left:
 # - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
 # - Current directory name
 # - Git branch and dirty state (if inside a git repo)
+
+set -g theme_prompt_char_normal '⟩ '
+set -g theme_prompt_virtual_env_char_begin '['
+set -g theme_prompt_virtual_env_char_end ']'
+set -g theme_prompt_git_char_begin '('
+set -g theme_prompt_git_char_end ')'
+set -g theme_prompt_git_is_dirty '±'
+set -g theme_prompt_divider ' · '
+set -g theme_prompt_newline yes
+set -g theme_prompt_full_path yes
 
 function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
@@ -14,16 +24,12 @@ function _git_is_dirty
 end
 
 function fish_prompt
-  set -l last_status $status
-
   set -l cyan (set_color cyan)
   set -l yellow (set_color yellow)
   set -l red (set_color red)
   set -l blue (set_color blue)
   set -l green (set_color green)
   set -l normal (set_color normal)
-
-  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
 
   # Output the prompt, left to right
 
@@ -32,30 +38,40 @@ function fish_prompt
 
   # Display [venvname] if in a virtualenv
   if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+      echo -n -s (set_color -b cyan black) $theme_prompt_virtual_env_char_begin (basename "$VIRTUAL_ENV") $theme_prompt_virtual_env_char_end $normal ' '
+  end
+
+  set -l pretty_path (pwd | sed "s:^$HOME:~:")
+
+  set -l visual_path $blue$pretty_path
+  if test $theme_prompt_full_path = no
+    set visual_path $blue(basename $pretty_path)
   end
 
   # Print pwd or full path
-  echo -n -s $cwd $normal
+  echo -n -s $visual_path $normal
 
   # Show git branch and status
   if [ (_git_branch_name) ]
     set -l git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info '(' $yellow $git_branch "±" $normal ')'
+      set git_info $theme_prompt_git_char_begin $yellow $git_branch $theme_prompt_git_is_dirty $normal $theme_prompt_git_char_end
     else
-      set git_info '(' $green $git_branch $normal ')'
+      set git_info $theme_prompt_git_char_begin $green $git_branch $normal $theme_prompt_git_char_end
     end
-    echo -n -s ' · ' $git_info $normal
+    echo -n -s $theme_prompt_divider $git_info $normal
   end
 
+  set -l last_status $status
   set -l prompt_color $red
   if test $last_status = 0
     set prompt_color $normal
   end
 
+  if test $theme_prompt_newline = yes
+    echo -e ''
+  end
   # Terminate with a nice prompt char
-  echo -e ''
-  echo -e -n -s $prompt_color '⟩ ' $normal
+  echo -e -n -s $prompt_color $theme_prompt_char_normal $normal
 end
